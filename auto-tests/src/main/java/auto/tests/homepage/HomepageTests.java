8c3e49a4dc6ce42a8946a320ee9e9791a0c742d1
NEERAVKR04 <30573865+NEERAVKR04@users.noreply.github.com>
2020-10-20 00:02:31 +0530

    maven plugin issues resolved

--- auto-tests/src/main/java/auto/tests/homepage/HomepageTests.java
+++ auto-tests/src/main/java/auto/tests/homepage/HomepageTests.java
@@ -1,4 +1,11 @@
 package auto.tests.homepage;
+import org.testng.annotations.Test;
+import auto.framework.core.BaseTest;
+import auto.page.homepage.homepage;
 
-public class HomepageTests {
+public class HomepageTests extends BaseTest{
+    @Test
+    public void HomepageTests() {
+        Homepage Homepage = new Homepage(getDriver());
+    }
 }
